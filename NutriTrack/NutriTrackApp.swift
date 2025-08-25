43cff1ef91e4a75df62abf822ec182865612b62b
Denis <deniska.shabanov.2019@mail.ru>
2025-08-25 14:25:11 +0300

    Initial Commit

---
+++ NutriTrack/NutriTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriTrackApp.swift
+//  NutriTrack
+//
+//  Created by Denis Shabanov on 25.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct NutriTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NutriTrackTests/NutriTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutriTrackTests.swift
+//  NutriTrackTests
+//
+//  Created by Denis Shabanov on 25.08.2025.
+//
+
+import Testing
+@testable import NutriTrack
+
+struct NutriTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
