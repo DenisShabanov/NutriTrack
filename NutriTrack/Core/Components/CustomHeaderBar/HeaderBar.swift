f93b8c381414b8345696a8942cc0aa7170ea6160
DenisShabanov <Shabik35@yandex.ru>
2025-09-25 15:36:39 +0300

    [Featute] Created a custom page header component, added a date formatter, and updated Home View.

---
+++ NutriTrack/Core/Components/CustomHeaderBar/HeaderBar.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderBar.swift
+//  NutriTrack
+//
+//  Created by Denis Shabanov on 24.09.2025.
+//
+
+import SwiftUI
+
+struct HeaderBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderBar()
+}
---
+++ NutriTrack/Core/Recipes/Views/RecipePageView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipePageView.swift
+//  NutriTrack
+//
+//  Created by Denis Shabanov on 24.09.2025.
+//
+
+import SwiftUI
+
+struct RecipePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipePageView()
+}
